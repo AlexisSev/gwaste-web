4d6836e7f2e89cd34dbfa151ab6d25c3bc8bb5c9
Leksis <alexiszkie3@gmail.com>
2025-09-22 21:25:50 +0800

    Enhances UIs, fixed some minor errors

--- src/pages/ViewMap.js
+++ src/pages/ViewMap.js
@@ -9,7 +9,7 @@ import "./ViewMap.css";
 // Fix for Leaflet icons
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  // iconRetinaUrl: require("leaflet/dist/images/  marker-icon-2x.png"),
   iconUrl: require("leaflet/dist/images/marker-icon.png"),
   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
